9fd935941c5277d1a9c2b834e9c203c7259946d5
Alex Yaroshuk <alexander_4288@Yahoo.com>
2024-04-23 02:42:09 +0800

    add stream support

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -13,10 +13,10 @@ final class ApiClient {
   ApiClient({required String apiKey}) : _apiKey = apiKey;
 
   // Send request to Groq API
-  Future<GroqResponse> makeRequest({
+    Future<GroqResponse> makeRequest({
     required GroqRequest groqRequest,
   }) async {
-    final response = await (http.post)(
+    final response = await http.post(
       _uri,
       headers: {
         ApiHeaderKeys.authorization: ApiHeaderValues.bearer(_apiKey),
@@ -26,13 +26,20 @@ final class ApiClient {
     );
 
     if (response.statusCode >= 200 && response.statusCode < 300) {
-      return GroqResponse.fromJson(
+      // Assuming the response is now a stream wrapped in "data" objects
+      final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+      if (decodedResponse.containsKey('data')) {
+        // If the response contains 'data', parse it
+        return GroqResponse.fromJson(decodedResponse['data']);
+      } else {
+        // If there's no 'data' key, the format is unexpected
+        throw FormatException('Expected response to contain "data" key.');
+      }
+    } else {
+      throw parseErrorFor(
+        response.statusCode,
         jsonDecode(response.body) as Map<String, dynamic>,
       );
     }
-    throw parseErrorFor(
-      response.statusCode,
-      jsonDecode(response.body) as Map<String, dynamic>,
-    );
   }
 }
